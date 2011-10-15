7c222d622747c161a984e87a0bda45f1f8ac3778
Vasil Rangelov <boen.robot@gmail.com>
2011-10-16 00:24:39 +0300

    Getting ready for release.

--- extrasetup.php
+++ extrasetup.php
@@ -3,10 +3,17 @@
 $transmitterPackage
     = new \PEAR2\Pyrus\Package(
         __DIR__ . DIRECTORY_SEPARATOR
-        . '../../PEAR2_Net_Transmitter.git/package.xml'
+        . '../PEAR2_Net_Transmitter.git/package.xml'
     );
 unset($transmitterPackage->files['docs/docblox.xml']);
 unset($transmitterPackage->files['docs/doxygen.ini']);
-unset($transmitterPackage->files['tests/configuration.xml']);
+
+unset($transmitterPackage->files['tests/ClientTest.php']);
+unset($transmitterPackage->files['tests/ServerTest.php']);
+unset($transmitterPackage->files['tests/UnconnectedTest.php']);
 unset($transmitterPackage->files['tests/bootstrap.php']);
+unset($transmitterPackage->files['tests/phpunit.xml']);
+unset($transmitterPackage->files['tests/secondaryPeer.xml']);
+unset($transmitterPackage->files['tests/secondaryPeer.bat']);
+
 $extrafiles = array($transmitterPackage);
