8ec6f27b08d9f9e4f35b7c53a933279bc24ebd3c
Vasil Rangelov <boen.robot@gmail.com>
2013-10-20 23:58:59 +0300

    Adjusted roscon.bat to match what's advertised in its REMs; Added a shebang to the stub, making the PHAR potentially directly executable on UNIX.

--- scripts/roscon.bat
+++ scripts/roscon.bat
@@ -1,34 +1,35 @@
 @echo off
-:: Prefer PHP binary in the following order:
-:: 1. Whatever %PHPBIN% points to.
-:: 2. "php" from %cd% with one of %pathext% extensions.
-:: 3. "php" from a %path% path with one of %pathext% extensions.
-:: 4. Whatever %PHP_PEAR_PHP_BIN% points to.
-::
-:: Once a binary is found, a file with the same name as this one
-:: will be ran. Prefered extensions are no extension and ".php"
-:: in this order.
-::
-if "%PHPBIN%" == "" set PHPBIN=php
-if exist "%PHPBIN%" goto SET_FILE
-if "%PHP_PEAR_PHP_BIN%" neq "" goto USE_PEAR_BIN
-goto PHP_ERR
-:USE_PEAR_BIN
-set PHPBIN=%PHP_PEAR_PHP_BIN%
-if exist "%PHP_PEAR_PHP_BIN%" goto SET_FILE
-goto PHP_ERR
-:SET_FILE
-if "%PHPFILE%" == "" set PHPFILE=%~d0%~p0%~n0
-if exist "%PHPFILE%" goto RUN
-set PHPFILE=%~d0%~p0%~n0.php
-if exist "%PHPFILE%" goto RUN
-goto FILE_ERR
-:RUN
-"%PHPBIN%" "%PHPFILE%" %*
-goto DONE
+REM Prefer PHP binary in the following order:
+REM 1. Whatever %PHPBIN% points to.
+REM 2. "php" from %cd% with one of %pathext% extensions.
+REM 3. "php" from a %path% path with one of %pathext% extensions.
+REM 4. Whatever %PHP_PEAR_PHP_BIN% points to.
+REM
+REM Once a binary is found, a file is looked for at the %PHPFILE% environment
+REM variable (not intendend to be overriden).
+REM Fallbacks to a file with the same name as this one.
+REM Prefered extensions are ".php" and then no extension.
+REM
+goto SET_BIN
 :PHP_ERR
-echo PHP interpreter not found. Please set the %%PHPBIN%% or %%PHP_PEAR_PHP_BIN%% environment variable to one, or add a PHP interpreter to your %%PATH%%.
+echo PHP interpreter not found. Please set the %%PHPBIN%% or %%PHP_PEAR_PHP_BIN%% environment variable to one, or add one to your %%PATH%%.
 goto DONE
 :FILE_ERR
-echo The file to be ran was not found. It should be in the same folder, and either have a ".php" extension, or no extension at all.
+echo The file to be ran was not found. It should be at either "%~d0%~p0%~n0.php" or "%~d0%~p0%~n0".
+goto DONE
+:SET_BIN
+if "%PHPBIN%" == "" set PHPBIN=php
+where /q %PHPBIN%
+if %ERRORLEVEL% == 0 goto SET_FILE
+if "%PHP_PEAR_PHP_BIN%" == "" goto PHP_ERR
+where /q "%PHP_PEAR_PHP_BIN%"
+if %ERRORLEVEL% neq 0 goto PHP_ERR
+set PHPBIN=%PHP_PEAR_PHP_BIN%
+:SET_FILE
+if "%PHPFILE%" == "" set PHPFILE=%~d0%~p0%~n0.php
+if exist "%PHPFILE%" goto RUN
+set PHPFILE=%~d0%~p0%~n0
+if not exist "%PHPFILE%" goto FILE_ERR
+:RUN
+"%PHPBIN%" "%PHPFILE%" %*
 :DONE
