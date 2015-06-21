9f76038096cd7acf5aab09290d88645c4cb7aa26
Vasil Rangelov <boen.robot@gmail.com>
2015-06-21 14:17:03 +0300

    Added Sami configuration file and .gitattributes that makes Composer ignore non-essentials when preferring distribution; All documentation generators now generate files in "Reference/{{generator}}/Doc".

---
+++ docs/sami.php
@@ -0,0 +1,20 @@
+<?php
+use Sami\Sami;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+
+return new Sami(
+    Finder::create()
+        ->files()
+        ->name('*.php')
+        ->exclude('docs')
+        ->exclude('examples')
+        ->exclude('tests')
+        ->in($dir = dirname(__DIR__)),
+    array(
+        'title'                => 'PEAR2_Net_RouterOS documentation',
+        'build_dir'            => __DIR__ . '/Reference/Sami/Doc',
+        'cache_dir'            => __DIR__ . '/Reference/Sami/Cache',
+        'default_opened_level' => 1
+    )
+);
